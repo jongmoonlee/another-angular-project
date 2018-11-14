5f0dd6ef6aa594c6f799da6e2b062817e34374fe
jongmoonlee <jmfromoz@gmail.com>
2018-11-14 23:05:33 +1100

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InterestsComponent } from './interests/interests.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterestsComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'interests', component: InterestsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
